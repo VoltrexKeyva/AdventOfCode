253f46c438f526c8858db952b0bb6f2f4e4ffb20
Mohammed Keyvanzadeh <mohammadkeyvanzade94@gmail.com>
2021-12-01 02:44:28 -0800

    2021: day 1 - puzzle 1
    Day 1, Puzzle 1 solved.

---
+++ 2021/day-1/solve-puzzle-1.mjs
@@ -0,0 +1,22 @@
+import { readFileSync } from 'node:fs';
+
+const input = readFileSync('./input.txt', 'utf-8').split('\n');
+const len = input.length;
+
+let timesIncreased = 0;
+
+for (let i = 0, first = true, previous = null; i < len; i++) {
+  if (first) {
+    first = false;
+    previous = Number(input[i]);
+    continue;
+  }
+
+  const num = Number(input[i]);
+
+  if (num > previous) timesIncreased++;
+
+  previous = num;
+}
+
+console.log(timesIncreased);
