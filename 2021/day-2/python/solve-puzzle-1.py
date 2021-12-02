295c12c8d53c879ba1cd089025f373a7f5e31ab7
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-12-02 20:56:02 +0700

    feat: add python

---
+++ 2021/day-2/python/solve-puzzle-1.py
@@ -0,0 +1,18 @@
+with open('../input.txt', 'r') as handle:
+    horizontal_position = 0
+    depth = 0
+
+    for instruction in handle.read().splitlines():
+        instruction_line = instruction.split(' ')
+        arg = int(instruction_line[1])
+        
+        instruction_line = instruction_line[0]
+        
+        if instruction_line == "forward":
+            horizontal_position += arg
+        elif instruction_line == "up":
+            depth -= arg
+        elif instruction_line == "down":
+            depth += arg
+    
+    print(horizontal_position * depth)
