a5ea86c049db525c615d6a9d50a9999ad66771e7
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-12-02 19:56:26 +0700

    fix: remove comments

--- 2021/day-1/python/solve-puzzle-2.py
+++ 2021/day-1/python/solve-puzzle-2.py
@@ -5,17 +5,15 @@ with open('../input.txt', 'r') as handle:
     times_increased = 0
     limit = len(text) - 2
     i = 1
-    #print(limit)
+    
     while i < limit:
         nums.append(int(text[i + 2]))
-        #print(i, end=">")
    
         current_sum = nums[i] + nums[i + 1] + nums[i + 2]
         if current_sum > previous_sum:
-            #print(len(nums), end=" ")
             times_increased += 1
         
         previous_sum = current_sum
         i += 1
-    #print()
+
     print(times_increased)
